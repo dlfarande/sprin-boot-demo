7ce811d11ca4a5a580681a5ef7553a34c3dbf2d1
dlf <dlfarande@gmail.com>
2024-02-19 14:22:45 +0530

    New Spring-boot Project commited

---
+++ src/main/java/com/sav16dlf/dlfapp/DlfappApplication.java
@@ -0,0 +1,13 @@
+package com.sav16dlf.dlfapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlfappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlfappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sav16dlf/dlfapp/DlfappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sav16dlf.dlfapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlfappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
